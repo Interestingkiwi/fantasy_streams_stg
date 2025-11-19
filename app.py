00b6d35f0816806be4f0ad666ff225fe2aa4aa15
Interestingkiwi <jasondruckenmiller@gmail.com>
2025-11-19 13:40:33 -0500

    typo

--- app.py
+++ app.py
@@ -2444,7 +2444,7 @@ def get_trade_helper_league_roster_data():
 
         # 6. Get Ranks AND Stats
         cat_rank_columns = [f"{cat}_cat_rank" for cat in all_scoring_categories]
-        raw_stat_columns = [f"'{cat}"' for cat in all_scoring_categories]
+        raw_stat_columns = [f'"{cat}"' for cat in all_scoring_categories]
         valid_normalized_names = [p.get('player_name_normalized') for p in all_players if p.get('player_name_normalized')]
 
         if valid_normalized_names:
